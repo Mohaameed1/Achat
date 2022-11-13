b654e66deb2c08e0a6c573aac4e6231ff87d4283
eya souissi <48189583+eyaaaaa@users.noreply.github.com>
2022-11-13 15:47:11 +0100

    Update SecteurActivite.java

--- src/main/java/com/esprit/examen/entities/SecteurActivite.java
+++ src/main/java/com/esprit/examen/entities/SecteurActivite.java
@@ -8,48 +8,17 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ALLArgsConstructor;
+
 @Entity
-
+@Getter
+@Setter
 @AllArgsConstructor
-
+@NoArgsConstructor
 public class SecteurActivite implements Serializable{
-	
-	public SecteurActivite() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	public Long getIdSecteurActivite() {
-		return idSecteurActivite;
-	}
-	public void setIdSecteurActivite(Long idSecteurActivite) {
-		this.idSecteurActivite = idSecteurActivite;
-	}
-	public String getCodeSecteurActivite() {
-		return codeSecteurActivite;
-	}
-	public void setCodeSecteurActivite(String codeSecteurActivite) {
-		this.codeSecteurActivite = codeSecteurActivite;
-	}
-	public String getLibelleSecteurActivite() {
-		return libelleSecteurActivite;
-	}
-	public void setLibelleSecteurActivite(String libelleSecteurActivite) {
-		this.libelleSecteurActivite = libelleSecteurActivite;
-	}
-	public Set<Fournisseur> getFournisseurs() {
-		return fournisseurs;
-	}
-	public void setFournisseurs(Set<Fournisseur> fournisseurs) {
-		this.fournisseurs = fournisseurs;
-	}
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
 	/**
 	 * 
 	 */
