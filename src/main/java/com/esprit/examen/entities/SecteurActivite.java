23101078ff732e73fab363c250f6d62aea694641
eya souissi <48189583+eyaaaaa@users.noreply.github.com>
2022-11-13 15:31:08 +0100

    Update SecteurActivite.java

--- src/main/java/com/esprit/examen/entities/SecteurActivite.java
+++ src/main/java/com/esprit/examen/entities/SecteurActivite.java
@@ -8,7 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
