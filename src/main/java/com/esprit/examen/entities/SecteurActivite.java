d3d7947ab0e80b72ce2f08dbbac326e8688a9357
eyaaaaa <eya.souissi@esprit.tn>
2022-11-13 14:41:03 +0100

    dev

---
+++ src/main/java/com/esprit/examen/entities/SecteurActivite.java
@@ -0,0 +1,72 @@
+package com.esprit.examen.entities;
+
+import java.io.Serializable;
+import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+
+@AllArgsConstructor
+
+public class SecteurActivite implements Serializable{
+	public SecteurActivite(Long idSecteurActivite, String codeSecteurActivite, String libelleSecteurActivite,
+			Set<Fournisseur> fournisseurs) {
+		super();
+		this.idSecteurActivite = idSecteurActivite;
+		this.codeSecteurActivite = codeSecteurActivite;
+		this.libelleSecteurActivite = libelleSecteurActivite;
+		this.fournisseurs = fournisseurs;
+	}
+	public SecteurActivite() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Long getIdSecteurActivite() {
+		return idSecteurActivite;
+	}
+	public void setIdSecteurActivite(Long idSecteurActivite) {
+		this.idSecteurActivite = idSecteurActivite;
+	}
+	public String getCodeSecteurActivite() {
+		return codeSecteurActivite;
+	}
+	public void setCodeSecteurActivite(String codeSecteurActivite) {
+		this.codeSecteurActivite = codeSecteurActivite;
+	}
+	public String getLibelleSecteurActivite() {
+		return libelleSecteurActivite;
+	}
+	public void setLibelleSecteurActivite(String libelleSecteurActivite) {
+		this.libelleSecteurActivite = libelleSecteurActivite;
+	}
+	public Set<Fournisseur> getFournisseurs() {
+		return fournisseurs;
+	}
+	public void setFournisseurs(Set<Fournisseur> fournisseurs) {
+		this.fournisseurs = fournisseurs;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idSecteurActivite;
+	private String codeSecteurActivite;
+	private String libelleSecteurActivite;
+	@ManyToMany(mappedBy="secteurActivites")
+	@JsonIgnore
+	private Set<Fournisseur> fournisseurs;
+}
